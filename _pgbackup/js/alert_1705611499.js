d01a1ab49c22ea619a73de049a1fef078580a20f
Alejandro <sashamaro91@gmail.com>
2024-01-18 22:00:29 +0100

    Adding alert JS file
    An alert to prevent user of the status of the website

---
+++ _pgbackup/js/alert_1705611499.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+  var message = "Hey, thanks for visiting! This website is still under construction, but you can explore and try my WebAR catalog in the Projects section. Visit the portfolio page here: https://joalander.github.io/all%20-projects.html ";
+  alert(message);
+};
